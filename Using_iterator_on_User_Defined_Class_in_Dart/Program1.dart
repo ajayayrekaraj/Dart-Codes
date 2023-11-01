1d61e9697b37300480ec88ef9c0c4e9c421579bf
ajayayrekaraj <ajayayrekaraj@gmail.com>
2023-11-01 23:42:10 +0530

    Dart Codes

---
+++ Using_iterator_on_User_Defined_Class_in_Dart/Program1.dart
@@ -0,0 +1,26 @@
+class ProgramLang implements Iterator {
+  int index = -1;
+  var proLang = [];
+
+  ProgramLang(String Lang) {
+    this.proLang = Lang.split(",");
+  }
+  bool moveNext() {
+    if (index < proLang.length - 1) {
+      index = index + 1;
+      return true;
+    }
+    return false;
+  }
+
+  get current {
+    if (index >= 0 && index <= proLang.length - 1) return proLang[index];
+  }
+}
+
+void main() {
+  ProgramLang obj = new ProgramLang("Cpp,Java,Python,Dart");
+  while (obj.moveNext()) {
+    print(obj.current);
+  }
+}
